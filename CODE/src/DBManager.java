a0b5ebc352d4e536ef4f787846161511f4a6aed7
Sarah Chataignier <sarahchataignier@gmail.com>
2021-10-19 20:33:58 +0200

    ajout dans la methode ProcessCommand

--- CODE/src/DBManager.java
+++ CODE/src/DBManager.java
@@ -1,3 +1,5 @@
+import java.util.StringTokenizer;
+
 public class DBManager {
     private static DBManager DBManager = new DBManager();
 
@@ -10,10 +12,14 @@ public class DBManager {
         BufferManager.getBufferManager().FlushAll();
     }
 
-    public void ProcessCommand(String ch){
-        //PASS
-    }
-
+    public void ProcessCommand(String ch) throws Exception{
+    	StringTokenizer st = new StringTokenizer(ch, " ");
+		if(st.nextToken().equals("CREATE") && st.nextToken().equals("TABLE") ){
+			CreateRelationCommand createRel = new CreateRelationCommand(ch);
+			createRel.Execute();
+		}
+	}
+		
     public static DBManager getDBManager(){
         return DBManager;
     }
