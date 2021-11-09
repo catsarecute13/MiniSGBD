47607703ea342ccfd2a1db3385add4d26810b011
Sarah Chataignier <sarahchataignier@gmail.com>
2021-11-09 10:41:42 +0100

    mon chemin en commentaire

--- CODE/src/DBParams.java
+++ CODE/src/DBParams.java
@@ -1,7 +1,7 @@
 
 public class DBParams {  //On la declare en final ? 
 	//public static String DBPath = "C:\\Users\\rahal\\DB";
-	public static String DBPath = "/users/licence/il02323/BDDA/test/";
+	//public static String DBPath = "C:\\Users\\sarah\\OneDrive\\Bureau";
 	public static int pageSize=4096;  
 	public static int maxPagesPerFile=4; 
 	public static int frameCount = 2;
